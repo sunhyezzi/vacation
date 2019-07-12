6a73ad68b46b2b44f05dd30869ecca3813c356a5
sunhyezzi <sunhye0710@gmail.com>
2019-07-13 02:08:51 +0900

    1_week

---
+++ myapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Myapp
+# Register your models here.
+admin.site.register(Myapp)
---
+++ myapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Myapp
+
+
+# Create your views here.
+
+def index(request):
+    myapps =Myapp.objects #쿼리셋
+    return render(request, 'index.html', {'myapps':myapps})
+
+def detail(request, myapp_id):
+    myapp_detail = get_object_or_404(Myapp, pk = myapp_id )
+    return render(request, 'detail.html', {'myapp':myapp_detail})
+
+def new(request): #new.html 띄워주는 함수
+    return render(request, 'new.html')
+
+def create(request): #입력받은 내용을 데이터베이스에 넣어주는 함수
+    myapp = Myapp()
+    myapp.title = request.GET['title']
+    myapp.body = request.GET['body']
+    myapp.update_date = timezone.datetime.now()
+    myapp.save()
+    return redirect('/myapp/' + str(myapp.id))
+
+def delete(request, pk):
+        myapp = Myapp.objects.get(id=pk)
+        myapp.delete()
+        return redirect('index')
