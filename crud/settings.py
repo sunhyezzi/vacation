5b128038804733d862d7aad4e7dd7902cdbafea8
sunhyezzi <sunhye0710@gmail.com>
2019-07-28 00:57:21 +0900

    last

--- crud/settings.py
+++ crud/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '4yi5&b_uu(sygi+nvhei_l-4_dws!s&%xfya5@hvm*%*#86**d'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
